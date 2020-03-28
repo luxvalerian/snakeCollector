8a098128f5c1c78e34fe53c639620b93aa6b73ab
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-28 18:43:40 -0500

    CBVs to create/delete/update

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('snakes/', views.snakes_index, name='index'),
+    path('snakes/<int:snake_id>/', views.snakes_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,27 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-class Snake:
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-snakes = [
-  Snake('Shoelace', 'corn snake', 'wildtype orange and black', 2),
-  Snake('Cotton Gin', 'western cottonmouth', 'dark, grayish-brown', 3),
-  Snake('Wallflower', 'texas rat snake', 'tan olive-green blotchy', 1)
-]
-
+from .models import Snake
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello ---===e </h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def snakes_index(request):
+    snakes = Snake.objects.all()
     return render(request, 'snakes/index.html', { 'snakes': snakes })
 
+def snakes_detail(request, snake_id):
+    snake = Snake.objects.get(id=snake_id)
+    return render(request, 'snakes/detail.html', { 'snakes': snakes })
