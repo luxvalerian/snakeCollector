b06c54601abcba7fcf5f560f78a78698486ea317
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-30 18:01:38 -0500

    add feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('S', 'Second Breakfast'),
+    ('E', 'Elevenses'),
+    ('L', 'Luncheon'),
+    ('A', 'Afternoon Tea'),
+    ('D', 'Dinner'),
+    ('S', 'Supper'),
+)
 
 # Create your models here.
 class Snake(models.Model):
@@ -9,4 +20,22 @@ class Snake(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'snake_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding datesss')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    snake = models.ForeignKey(Snake, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+
 
