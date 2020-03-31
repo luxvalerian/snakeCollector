bd6ef4395767076682a867fcc2e63b8a130381f6
Avelynn Mitra <avelynnmitra@gmail.com>
2020-03-30 21:34:52 -0500

    path add feeding

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('snakes/create/', views.SnakeCreate.as_view(), name='snakes_create'),
     path('snakes/<int:pk>/update/', views.SnakeUpdate.as_view(), name='snakes_update'),
     path('snakes/<int:pk>/delete/', views.SnakeDelete.as_view(), name='snakes_delete'),
+    path('snakes/<int:snake_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 ]
